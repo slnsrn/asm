ab875b2d34a4b7535ac04fb6d2154b03f874dfa6
slnsrn <slnsrn@gmail.com>
2023-05-09 13:28:56 +0200

    update names

--- _next/static/webpack/static/development/pages/index.js.60b24b44cce4105cc88e.hot-update.js
+++ _next/static/webpack/static/development/pages/index.js.60b24b44cce4105cc88e.hot-update.js
@@ -1,153 +1,220 @@
-webpackHotUpdate("static/development/pages/index.js",{
-
-/***/ "./components/OurTeam.js":
-/*!*******************************!*\
+webpackHotUpdate("static/development/pages/index.js", {
+  /***/ "./components/OurTeam.js":
+    /*!*******************************!*\
   !*** ./components/OurTeam.js ***!
   \*******************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+    /*! exports provided: default */
+    /***/ function (module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ =
+        __webpack_require__(/*! react */ "./node_modules/react/index.js");
+      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default =
+        /*#__PURE__*/ __webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+      /* harmony import */ var _withSectionLayout__WEBPACK_IMPORTED_MODULE_1__ =
+        __webpack_require__(
+          /*! ./withSectionLayout */ "./components/withSectionLayout.js"
+        );
+      var _this = undefined,
+        _jsxFileName =
+          "/Users/slnsrn/Documents/Projects/PelinAsm/components/OurTeam.js";
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _withSectionLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./withSectionLayout */ "./components/withSectionLayout.js");
-var _this = undefined,
-    _jsxFileName = "/Users/slnsrn/Documents/Projects/PelinAsm/components/OurTeam.js";
+      var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
+      var OurTeam = function OurTeam() {
+        return __jsx(
+          "div",
+          {
+            id: "our-team",
+            __self: _this,
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 4,
+              columnNumber: 3,
+            },
+          },
+          __jsx(
+            "h3",
+            {
+              className:
+                "font-thin text-4xl md:text-6xl text-center w-full mb-6",
+              __self: _this,
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 5,
+                columnNumber: 5,
+              },
+            },
+            "Ekibimiz"
+          ),
+          __jsx(
+            "div",
+            {
+              className: "flex flex-row flex-wrap",
+              __self: _this,
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 6,
+                columnNumber: 5,
+              },
+            },
+            __jsx(
+              "div",
+              {
+                className: "w-full md:w-1/2 p-4",
+                __self: _this,
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 7,
+                  columnNumber: 7,
+                },
+              },
+              __jsx("img", {
+                src: "doctor.jpeg",
+                alt: "Pelin Akgün",
+                __self: _this,
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 8,
+                  columnNumber: 7,
+                },
+              }),
+              __jsx(
+                "div",
+                {
+                  className:
+                    "bg-dark-blue p-4 text-white text-center tracking-wide",
+                  __self: _this,
+                  __source: {
+                    fileName: _jsxFileName,
+                    lineNumber: 9,
+                    columnNumber: 7,
+                  },
+                },
+                __jsx(
+                  "div",
+                  {
+                    className: "text-xl font-light ",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 10,
+                      columnNumber: 9,
+                    },
+                  },
+                  "Dr. Pelin Akgün"
+                ),
+                __jsx(
+                  "div",
+                  {
+                    className: "text-base font-thin my-2",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 11,
+                      columnNumber: 9,
+                    },
+                  },
+                  " 14.01.034 No.lu Aile Hekimli\u011Fi Birimi "
+                ),
+                __jsx(
+                  "div",
+                  {
+                    className: "text-medium",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 12,
+                      columnNumber: 9,
+                    },
+                  },
+                  "Aile Hekimi"
+                )
+              )
+            ),
+            __jsx(
+              "div",
+              {
+                className: "w-full md:w-1/2 p-4",
+                __self: _this,
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 15,
+                  columnNumber: 7,
+                },
+              },
+              __jsx("img", {
+                src: "nurse.jpeg",
+                alt: "Aysegul Bayram",
+                __self: _this,
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 16,
+                  columnNumber: 7,
+                },
+              }),
+              __jsx(
+                "div",
+                {
+                  className:
+                    "bg-dark-blue p-4 text-white text-center tracking-wide",
+                  __self: _this,
+                  __source: {
+                    fileName: _jsxFileName,
+                    lineNumber: 17,
+                    columnNumber: 7,
+                  },
+                },
+                __jsx(
+                  "div",
+                  {
+                    className: "tetext-xl font-light",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 18,
+                      columnNumber: 9,
+                    },
+                  },
+                  "Hem. Ay\u015Feg\xFCl BAYRAM"
+                ),
+                __jsx(
+                  "div",
+                  {
+                    className: "text-base font-thin my-2",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 19,
+                      columnNumber: 9,
+                    },
+                  },
+                  "14.01.034 No.lu Aile Hekimli\u011Fi Birimi "
+                ),
+                __jsx(
+                  "div",
+                  {
+                    className: "text-medium",
+                    __self: _this,
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 20,
+                      columnNumber: 9,
+                    },
+                  },
+                  "Hem\u015Fire"
+                )
+              )
+            )
+          )
+        );
+      };
 
-var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+      /* harmony default export */ __webpack_exports__["default"] = Object(
+        _withSectionLayout__WEBPACK_IMPORTED_MODULE_1__["default"]
+      )(OurTeam);
 
-
-var OurTeam = function OurTeam() {
-  return __jsx("div", {
-    id: "our-team",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 4,
-      columnNumber: 3
-    }
-  }, __jsx("h3", {
-    className: "font-thin text-4xl md:text-6xl text-center w-full mb-6",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 5,
-      columnNumber: 5
-    }
-  }, "Ekibimiz"), __jsx("div", {
-    className: "flex flex-row flex-wrap",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6,
-      columnNumber: 5
-    }
-  }, __jsx("div", {
-    className: "w-full md:w-1/2 p-4",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 7,
-      columnNumber: 7
-    }
-  }, __jsx("img", {
-    src: "doctor.jpeg",
-    alt: "Pelin Serin",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 8,
-      columnNumber: 7
-    }
-  }), __jsx("div", {
-    className: "bg-dark-blue p-4 text-white text-center tracking-wide",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 9,
-      columnNumber: 7
-    }
-  }, __jsx("div", {
-    className: "text-xl font-light ",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 10,
-      columnNumber: 9
-    }
-  }, "Dr. Pelin Serin"), __jsx("div", {
-    className: "text-base font-thin my-2",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 11,
-      columnNumber: 9
-    }
-  }, " 14.01.034 No.lu Aile Hekimli\u011Fi Birimi "), __jsx("div", {
-    className: "text-medium",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 12,
-      columnNumber: 9
-    }
-  }, "Aile Hekimi"))), __jsx("div", {
-    className: "w-full md:w-1/2 p-4",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 15,
-      columnNumber: 7
-    }
-  }, __jsx("img", {
-    src: "nurse.jpeg",
-    alt: "Aysegul Bayram",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 16,
-      columnNumber: 7
-    }
-  }), __jsx("div", {
-    className: "bg-dark-blue p-4 text-white text-center tracking-wide",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 17,
-      columnNumber: 7
-    }
-  }, __jsx("div", {
-    className: "tetext-xl font-light",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 18,
-      columnNumber: 9
-    }
-  }, "Hem. Ay\u015Feg\xFCl BAYRAM"), __jsx("div", {
-    className: "text-base font-thin my-2",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 19,
-      columnNumber: 9
-    }
-  }, "14.01.034 No.lu Aile Hekimli\u011Fi Birimi "), __jsx("div", {
-    className: "text-medium",
-    __self: _this,
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 20,
-      columnNumber: 9
-    }
-  }, "Hem\u015Fire")))));
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(_withSectionLayout__WEBPACK_IMPORTED_MODULE_1__["default"])(OurTeam));
-
-/***/ })
-
-})
+      /***/
+    },
+});
 //# sourceMappingURL=index.js.60b24b44cce4105cc88e.hot-update.js.map
